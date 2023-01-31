45d6450402ff051669fa32fd3fc5ad72f5735e7a
vickyramz22 <111348402+vickyramz22@users.noreply.github.com>
2023-01-31 13:14:23 +0530

    Update Package.swift
    earthId name changes

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Veriff",
+    name: "EarthId",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         .library(
-            name: "Veriff",
-            targets: ["Veriff"]),
+            name: "EarthId",
+            targets: ["EarthId"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "Veriff",
+        .binaryTarget(name: "EarthId",
                       url: "https://cdn.veriff.me/ios/com/veriff/veriffsdk/6.1.0/veriffsdk-6.1.0.zip",
                       checksum: "6d0638380c00ec1a6773c927e4b0df03ad998ffff93929d97d837867e84cd4c2"),
     ]
