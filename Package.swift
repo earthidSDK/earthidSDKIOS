9de680252a694438dfc1f08cf2927f292e2e0827
vigneshwar <vickyrams20@gmail.com>
2023-02-02 13:28:35 +0530

    Update Package.swift
    Veriff changed

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "EarthId",
-            targets: ["EarthId"]),
+            name: "Veriff",
+            targets: ["Veriff"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "EarthId",
+        .binaryTarget(name: "Veriff",
                       url: "https://cdn.veriff.me/ios/com/veriff/veriffsdk/6.1.0/veriffsdk-6.1.0.zip",
                       checksum: "6d0638380c00ec1a6773c927e4b0df03ad998ffff93929d97d837867e84cd4c2"),
     ]
